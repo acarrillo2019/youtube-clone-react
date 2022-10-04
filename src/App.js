13ac93187b32f9c93c2d44552d758bce33efc23b
Alfredo Carrillo <alric21@aol.com>
2022-10-04 06:58:13 -0700

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const app = () => {
-  return (
-    <div>App</div>
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' element={<Feed />} />
+                <Route path='/video/:id' element={<VideoDetail />} />
+                <Route path='/channel/:id' element={<ChannelDetail />} />
+                <Route path='/search/:searchTerm' element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
-}
 
-export default app
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
